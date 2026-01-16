ca987f1f1dcd3cc5b95dd4ad2705ba2d8fabab8e
nattanjunior <fnatanieljunior@gmail.com>
2026-01-16 17:23:34 -0300

    feat: add Swagger documentation setup for API
    Integrate Swagger UI for API documentation at '/docs' endpoint to improve developer experience and API discoverability.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+   const config = new DocumentBuilder()
+    .setTitle('Moti-Me API')
+    .setDescription('The Moti-Me API description')
+    .setVersion('1.0')
+    .addTag('moti-me')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
