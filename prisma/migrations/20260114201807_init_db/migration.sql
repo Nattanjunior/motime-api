889fa3e1af860e0319239b24da9f929f3edc5948
nattanjunior <fnatanieljunior@gmail.com>
2026-01-16 13:00:55 -0300

    feat(db): add initial database schema and migrations
    - Initialize database with User and Payment models
    - Add email field with unique constraint to User model
    - Set up migration lock file for postgresql provider

---
+++ prisma/migrations/20260114201807_init_db/migration.sql
@@ -0,0 +1,22 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Payment" (
+    "id" TEXT NOT NULL,
+    "price" DECIMAL(65,30) NOT NULL,
+    "status" BOOLEAN NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Payment_pkey" PRIMARY KEY ("id")
+);
