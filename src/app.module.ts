3050c0761cf83f77b1475bad9882bfc2aaeb7e61
nattanjunior <fnatanieljunior@gmail.com>
2026-01-23 16:28:31 -0300

    feat(app): integrate Prisma and Auth modules with controllers and services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './infra/database/prisma/prisma.module';
+import { AuthModule } from './application/use-cases/auth/auth.module';
+import { AuthController } from './infra/http/controllers/auth.controller';
+import { SubscriptionsController } from './infra/http/controllers/subscriptions.controller';
+import { SubscriptionRepository } from './domain/prisma/subscription.repository';
+import { CreatedSubsUseCase } from './application/use-cases/subscription/createdSubsUseCase';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [PrismaModule, AuthModule],
+  controllers: [AppController, AuthController, SubscriptionsController],
+  providers: [AppService, SubscriptionRepository, CreatedSubsUseCase],
 })
 export class AppModule {}
