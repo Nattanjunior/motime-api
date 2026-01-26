3f27dde3ffcb923f5fdbe91afae037f3c8695d00
nattanjunior <fnatanieljunior@gmail.com>
2026-01-26 16:11:40 -0300

    feat(main): add global exception filters and enhance Swagger setup

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import {
+  HttpExceptionFilter,
+  AllExceptionsFilter,
+} from './shared/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Exception filters
+  app.useGlobalFilters(new AllExceptionsFilter(), new HttpExceptionFilter());
+
+  // Swagger setup
   const config = new DocumentBuilder()
     .setTitle('Moti-Me API')
     .setDescription('The Moti-Me API description')
     .setVersion('1.0')
+    .addBearerAuth()
     .addTag('moti-me')
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
